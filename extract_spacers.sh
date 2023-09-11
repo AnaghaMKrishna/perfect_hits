f26c4a4c3dfc3996490c7460e65dbb316c25623b
Anagha <anaghakrishna92@gmail.com>
2023-09-10 21:08:35 -0400

    Extact start and stop positions of subject seq into new file

--- extract_spacers.sh
+++ extract_spacers.sh
@@ -5,8 +5,12 @@
 # subject files and  outputs the standard 12 columns and aligned subsequence.
 # Use awk for column-based filtering to extract rows which match 100%
 # and all 28 characters in CRISPR sequence and redirect to output file 
-blastn -query $1 -subject $2 -task blastn-short -outfmt '6 std sseq' |
-awk '{if ($3 == 100.000 && $4 == 28) print $0;}' > $3
+blastn -query $1 -subject $2 -task blastn-short -outfmt '6 std qlen' |
+awk '{if ($3 == 100.000 && $4 == $13) print $0;}' > $3
+
+cut -f 9,10 op.txt > op1.txt 
+sed 's/\t/\n/g' op1.txt > op2.txt
+cat op2.txt
 
 # Count the number of lines in output file to obtain number of perfect matches
-wc -l $3 
+#wc -l $3 
