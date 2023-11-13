6663ef68e85d04d2689603d71cf4f8b516c084bd
Anagha <anaghakrishna92@gmail.com>
2023-11-12 22:59:12 -0500

    corrected seqID for blast hits

--- extract_spacers.sh
+++ extract_spacers.sh
@@ -25,7 +25,9 @@ end=$(wc -l spacer_seq_manipulation.txt | cut -d ' ' -f 1)
 end=$((end/2))
 
 # Add header from matched file to the 1st column "end" number of times
-yes $(grep -Po -m 1 "(?<=>).*" $2) | head -n $end > col1.txt
+#yes $(grep -Po -m 1 "(?<=>).*" $2) | head -n $end > col1.txt
+#yes "NODE_18_length_84619_cov_36.005061" | head -n $end > col1.txt
+cut -f 2 blast_output.txt | head -n $(($end - 2)) > col1.txt
 
 # Form 2 columns of the start and end positions
 cat spacer_seq_manipulation.txt | paste col1.txt - - > spacer.bed
@@ -34,8 +36,11 @@ cat spacer_seq_manipulation.txt | paste col1.txt - - > spacer.bed
 # the start and end positions of spacers
 awk -i inplace '{$2=$2+1 ; $3=$3-1 ; print $0}' OFS='\t' spacer.bed 
 
+#Alternatively, we can also use single-line awk to create BED file with spacer co-ordinates
+#awk 'BEGIN {last=-1000} $9<last+50 {print $2, last, $9-1}; {last=$10}' blast_output.txt > $spacer.bed
+
 # Using seqtk to extract spacer sequences
 seqtk subseq $2 spacer.bed > $3
 
 # Output spacer sequences to STDOUT after removing header lines 
-cat $3 | grep -v ">"
+#cat $3 | grep -v ">" 
